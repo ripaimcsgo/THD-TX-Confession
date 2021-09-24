47316fe68ae3c2c916a0f610df903acc86eca6b1
hoan111 <hoan489@gmail.com>
2021-09-24 10:19:40 +0700

    UPDATE new files

--- src/client.php
+++ src/client.php
@@ -1,13 +1,26 @@
 <?php
 class client {
-    public function getClient()
+    public function SheetClient()
     {
         $client = new \Google_Client();
-        $client->setApplicationName("Web PHP tuong tac voi google sheet API");
+        $client->setApplicationName("Google sheet API client");
         $client->setScopes(Google\Service\Sheets::SPREADSHEETS);
         $client->setAuthConfig('../include/credentials.json');
         $client->setAccessType('offline');
         
+        return $client;
+    }
+
+    public function DriveClient()
+    {
+        $client = new Google_Client();
+        $client->setApplicationName('Google Drive API client');
+        $client->setScopes(Google_Service_Drive::DRIVE_FILE);
+        $client->setAuthConfig('../include/credentials.json');
+        $client->setAccessType('offline');
+        $client->setRedirectUri('http://localhost/cfs/THD-TX-Confession');
+
+
         return $client;
     }
 }

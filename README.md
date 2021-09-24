68b83fe7d167e7be3cd6b57dacd22f04581a320c
hoan111 <hoan489@gmail.com>
2021-09-24 18:32:01 +0700

    UPDATE new methods to interact with Google Drive

--- README.md
+++
@@ -1,7 +0,0 @@
-# THD TX Confession
-
-Đây là một hệ thống chia sẻ tâm tư ẩn danh 
-
-```
-Đang được nghiên cứu và phát triển và cũng là project mình dùng để học PHP :P 
-```
--- src/FormDataHandler.php
+++ src/FormDataHandler.php
@@ -8,41 +8,39 @@ $input = $_POST["content"];
 $upload_date = date('d/m/Y H:i:s');
 
 $send = new sendData();
-//$send = $send->uploadFrom($upload_date, $input, "");
+$send = $send->uploadFrom($upload_date, $input, "");
 
 //Form validation (copy from W3Schools)
-function test_input($data) {
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
-  }
+function test_input($data)
+{
+	$data = trim($data);
+	$data = stripslashes($data);
+	$data = htmlspecialchars($data);
+	return $data;
+}
 
-  //File upload handle
-  if (isset($_POST['submit'])) {
-		//$noi_dung = $_POST['fileToUpload'];
-		// Lay ten anh
-  		$image = $_FILES['fileToUpload']['name'];
+//File upload handle
+if (isset($_POST['submit'])) {
+	//$noi_dung = $_POST['fileToUpload'];
+	// Lay ten anh
+	$image = $_FILES['fileToUpload']['name'];
 
-  		// Lay phan duoi cua file
-  		$extension = substr($image, strpos($image, '.') + 1);
+	// Lay phan duoi cua file
+	$extension = substr($image, strpos($image, '.') + 1);
 
-  		// Noi luu anh
-  		$filename = basename($image);
-      $path = "E:\code\wamp_server\www\cfs\THD-TX-Confession\uploads/".$filename;
+	// Noi luu anh
+	$filename = basename($image);
+	$path = "E:\code\wamp_server\www\cfs\THD-TX-Confession\uploads/" . $filename;
 
-  		// Kiem tra xem co phai file anh hay khong
-  		if( $extension == "jpg" || $extension == "jpeg" || $extension == "png" ) {
-  			// Chuyen anh vao thu muc uploads
-  			move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $path);
-  		}else{
-  			$_SESSION["ErrorMess"] = "File bạn tải lên không phải là file ảnh";
-  		}
+	// Kiem tra xem co phai file anh hay khong
+	if ($extension == "jpg" || $extension == "jpeg" || $extension == "png") {
+		// Chuyen anh vao thu muc uploads
+		move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $path);
+	} else {
+		$_SESSION["ErrorMess"] = "File bạn tải lên không phải là file ảnh";
+	}
 
-  		// Encode the image string data into base64
-		$image_base64 = base64_encode($image);
-  }
-
-  //Testing google drive list files
-	$send->listFile();
+	// Encode the image string data into base64
+	$image_base64 = base64_encode($image);
+}
 ?>
